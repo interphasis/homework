4287d5c8f5ab6dde0516931aeb5828d83c6c2aac
interphasis <152515989+interphasis@users.noreply.github.com>
2026-03-22 19:16:31 +0300

    htmlcss

---
+++ htmlcss/js/sale.js
@@ -0,0 +1,8 @@
+
+
+const buttonCloser = document.querySelector('.sale__closer-btn');
+const sale = document.querySelector('.sale');
+
+buttonCloser.addEventListener('click', () => {
+  sale.classList.add('sale_close');
+});
