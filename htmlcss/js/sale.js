7fa33132901c9e6c299bd1d49ec812db761e3f8a
interphasis <152515989+interphasis@users.noreply.github.com>
2026-03-27 17:52:50 +0300

    Delete htmlcss directory

--- htmlcss/js/sale.js
+++
@@ -1,8 +0,0 @@
-
-
-const buttonCloser = document.querySelector('.sale__closer-btn');
-const sale = document.querySelector('.sale');
-
-buttonCloser.addEventListener('click', () => {
-  sale.classList.add('sale_close');
-});
