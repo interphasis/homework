1c7858bf2e579c231c0ca6eb1e37ca04d944f85a
interphasis <152515989+interphasis@users.noreply.github.com>
2026-03-31 13:43:40 +0300

    htmlcssjs

---
+++ htmlcssjs/js/slider.js
@@ -0,0 +1,37 @@
+export default function slider() {
+document.addEventListener('DOMContentLoaded', function() {
+
+  
+  const swiper = new Swiper('.feedback__slider', {
+  
+    grabCursor: true,
+    centeredSlides: false,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+   
+    mousewheel: {
+      forceToAxis: true,
+    },
+    
+    speed: 400,
+     breakpoints: {
+    360: {
+      slidesPerView: 1,
+    },
+    760: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    }
+  }
+  });
+  
+});
+}
