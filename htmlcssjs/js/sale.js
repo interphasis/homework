6d7cbb289ab97349f27eb7e3fd46b5fa17e604c0
interphasis <152515989+interphasis@users.noreply.github.com>
2026-04-06 08:19:36 +0300

    Delete htmlcssjs directory

--- htmlcssjs/js/sale.js
+++
@@ -1,9 +0,0 @@
-export default function saleModal() {
-
-const buttonCloser = document.querySelector('.sale__closer-btn');
-const sale = document.querySelector('.sale');
-
-buttonCloser.addEventListener('click', () => {
-  sale.classList.add('sale_close');
-});
-}
